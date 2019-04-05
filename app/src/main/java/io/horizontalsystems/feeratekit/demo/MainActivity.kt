24dc585a80ec3831903f0597a7fc02b491167135
Talgat Medetbekov <medetbekov.t@gmail.com>
2019-04-05 11:16:41 +0600

    Rename demo app package name

--- app/src/main/java/io/horizontalsystems/feeratekit/demo/MainActivity.kt
+++ app/src/main/java/io/horizontalsystems/feeratekit/demo/MainActivity.kt
@@ -1,7 +1,7 @@
-package io.horizontalsystems.feeratekit
+package io.horizontalsystems.feeratekit.demo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
