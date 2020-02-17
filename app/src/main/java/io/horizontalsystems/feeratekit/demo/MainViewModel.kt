4bb9aad968bcf8b00f3bee0d6fd1be7f3df65921
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-02-17 11:36:44 +0600

    Set new BTC fee rate provider

--- app/src/main/java/io/horizontalsystems/feeratekit/demo/MainViewModel.kt
+++ app/src/main/java/io/horizontalsystems/feeratekit/demo/MainViewModel.kt
@@ -2,12 +2,12 @@ package io.horizontalsystems.feeratekit.demo
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import io.horizontalsystems.feeratekit.model.FeeRate
 import io.horizontalsystems.feeratekit.FeeRateKit
 import io.horizontalsystems.feeratekit.model.Coin
 import io.horizontalsystems.feeratekit.model.FeeProviderConfig
-import io.reactivex.schedulers.Schedulers
+import io.horizontalsystems.feeratekit.model.FeeRate
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 
 class MainViewModel : ViewModel(){
@@ -19,7 +19,9 @@ class MainViewModel : ViewModel(){
             "2a1306f1d12f4c109a4d4fb9be46b02e",
             "fc479a9290b64a84a15fa6544a130218",
             "",
-            "https://damp-old-pond.quiknode.io/38708434-ee69-4c9a-84d7-cb0f7f45f2cc/YiBzRob3cfnxTRSvByiyFh2bU93pKzxeyyTHpacaaPF0YnCg9u_cxvvoPIC-3wh6eaQAPyZh5Hd-fDjLGFXCIA==/"
+            "http://134.209.138.9/fee_low/0ab6af109397934023af7eca5ec19e5f",
+            "http://134.209.138.9/fee_avg/e3cc8f8c828dddfd02914a74f0287b6c",
+            "http://134.209.138.9/fee_high/4d34b0a386827c81ef966e3af78712a6"
         ),
         App.instance
     )
