4b2657f2572487fe9d2785d1052ccd7a52e24794
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-02-18 14:20:14 +0600

    Use network config to use Http requests

--- app/src/main/java/io/horizontalsystems/feeratekit/demo/MainViewModel.kt
+++ app/src/main/java/io/horizontalsystems/feeratekit/demo/MainViewModel.kt
@@ -19,9 +19,9 @@ class MainViewModel : ViewModel(){
             "2a1306f1d12f4c109a4d4fb9be46b02e",
             "fc479a9290b64a84a15fa6544a130218",
             "",
-            "http://134.209.138.9/fee_low/0ab6af109397934023af7eca5ec19e5f",
-            "http://134.209.138.9/fee_avg/e3cc8f8c828dddfd02914a74f0287b6c",
-            "http://134.209.138.9/fee_high/4d34b0a386827c81ef966e3af78712a6"
+            "http://134.209.138.9/fee_low",
+            "http://134.209.138.9/fee_avg",
+            "http://134.209.138.9/fee_high"
         ),
         App.instance
     )
